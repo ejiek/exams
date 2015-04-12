8a138523c2e1d18ef8e15c109c775cf4eeede732
Vlad <ejiek@mail.ru>
2015-04-12 09:37:28 +0000

    login function now works with session

--- index.php
+++ index.php
@@ -14,10 +14,21 @@ include_once "connect_server.php";
     </head>
     
     <body>
-        <form class="bord" method="post" action="login.php" autocomplete="off">
-		<p><input type="text" name="username" placeholder="Username"></p>
-		<input type="password" name="password" placeholder="Password">
-		<div><input type="submit" value="Login">
-		<input type="submit" value="Sign Up"></div></form></li>
+        <?php
+        
+        session_start();
+        echo "session['user_id'] = ";
+        echo $_SESSION["user_id"];
+        if($_SESSION["user_id"] == 0){
+            echo '<form class="bord" method="post" action="login.php">';
+    		echo '<p><input type="text" name="username" placeholder="Username"></p>';
+    		echo '<input type="password" name="password" placeholder="Password">';
+    		echo '<div><input type="submit" value="Login">';
+    		echo '<input type="submit" value="Sign Up"></div></form></li>';
+        }
+        else{
+            echo '<a href="logout.php">Log Out</a>';
+        }
+		?>
     </body>
 </html>
--- login.php
+++ login.php
@@ -11,20 +11,22 @@ include_once "connect_server.php";
     $username=$_POST['username'];
     
 	// SQL-запрос
-	$strSQL = "SELECT user_id, login, password FROM " . login_dbt . " WHERE login = 'Student'";
+	$strSQL = "SELECT user_id, login, password FROM " . login_dbt . " WHERE login = '$username'";
 	$rs = mysql_query($strSQL);
     echo $rs;
+    $row = mysql_fetch_array($rs);
+    
     // Проверка наличия такого логина + пароля
-    if ($rows = mysql_fetch_row($rs)){
-        if ($row['password']!=$_POST["password"]){
-            echo $row;
-            echo "not euqval";
+    if ($row != 0){
+        if ($row['password']==$_POST["password"]){
+            session_start();
+            $_SESSION["user_id"] = $row['user_id'];
+            echo "session created";
         }
         else{
-            session_start();
-            $_SESSION["user_id"] = $rs['user_id'];
-            echo "good";
-            echo $row['user_id'];
+            echo "Error! Wrong Password</br>";
+            echo "p" . $_POST["password"] . "</br>";
+            echo "r" . $row['password'] . "</br>";
         }
 
     }
