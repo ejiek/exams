70c065a00423773d863e9aeb0f1b8d73097e1e22
Vlad <ejiek@mail.ru>
2015-04-12 08:37:17 +0000

    changes variabels fo connection some junk about fetching rows

--- login.php
+++ login.php
@@ -2,32 +2,34 @@
 include_once "connect_server.php";
 
 	// Соединение с сервером БД
-	mysql_connect($servername, $server_username, $server_password) or die (mysql_error ());
+	mysql_connect(servername, server_username, server_password) or die (mysql_error ());
 
 	// Выбор БД
-	mysql_select_db($db) or die(mysql_error());
+	mysql_select_db(db) or die(mysql_error());
 
     //БЕЛЫЙ СПИСОК?! ПЛЭЙСХОЛДЕРЫ?!
     $username=$_POST['username'];
     
-
 	// SQL-запрос
-	$strSQL = "SELECT * FROM $login_dbt WHERE username='$username'";
+	$strSQL = "SELECT user_id, login, password FROM " . login_dbt . " WHERE login = 'Student'";
 	$rs = mysql_query($strSQL);
-	$rows=$rs->sql_numrows();
-	
+    echo $rs;
     // Проверка наличия такого логина + пароля
-    if ($rows==0){
-        echo "Error!";
-    }
-    else{
-        if ($rs['password']!=$_POST["password"]){
-            echo "Error!";
+    if ($rows = mysql_fetch_row($rs)){
+        if ($row['password']!=$_POST["password"]){
+            echo $row;
+            echo "not euqval";
         }
         else{
             session_start();
             $_SESSION["user_id"] = $rs['user_id'];
+            echo "good";
+            echo $row['user_id'];
         }
+
+    }
+    else{
+        echo "Error! There is no such user";
     }
 
 
