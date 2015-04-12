06aa0a7581e07b90f3e673a454b458ed26c18b30
Vlad <ejiek@mail.ru>
2015-04-12 08:35:51 +0000

    changed the defenition type

--- connect_server.php
+++ connect_server.php
@@ -1,13 +1,13 @@
 <?php
-	define($servername, "localhost");
-	define($server_username, "polyexam");
-	define($server_password, "ChangeTh1s");
-	define($db, "polyexam");
-	define($login_dbt, "login");
-	define($user_dbt, "user");
-	define($exam_dbt, "exam");
-	define($question_dbt, "question");
-	define($answer_dbt, "answer");
-	define($user_exam_dbt, "user_exam");
-	define($user_answer_dbt, "user_answer");
+	define('servername', 'localhost');
+	define('server_username', 'polyexam');
+	define('server_password', 'ChangeTh1s');
+	define('db', 'polyexam');
+	define('login_dbt', 'login');
+	define('user_dbt', 'user');
+	define('exam_dbt', 'exam');
+	define('question_dbt', 'question');
+	define('answer_dbt', 'answer');
+	define('user_exam_dbt', 'user_exam');
+	define('user_answer_dbt', 'user_answer');
 ?>
