ede2f618ced2a915af91b3facda2ba68786cdb0c
Vlad <ejiek@mail.ru>
2015-04-23 07:41:30 +0000

    deletes more useless ";"

--- new_exam/create.php
+++ new_exam/create.php
@@ -8,7 +8,7 @@
 	mysql_select_db(db) or die(mysql_error());
         
     	$strSQL = "INSERT INTO `" . db . "`.`" . exam_dbt . "` (`id`,`Name`,`author_id`,`is_open`,`comment`) ";
-	    $strSQL = $strSQL .  "VALUES (NULL, '"  . $_POST["name"] . "', '"  . $_POST["author_id"] . "', '"  . $_POST["is_open"] . "', '"  . $_POST["comment"] . "');";
+	    $strSQL = $strSQL .  "VALUES (NULL, '"  . $_POST["name"] . "', '"  . $_POST["author_id"] . "', '"  . $_POST["is_open"] . "', '"  . $_POST["comment"] . "')";
 	    echo $strSQL;
 	    $rs = mysql_query($strSQL);
 	    
