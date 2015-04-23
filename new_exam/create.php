e7d9df98e77e0bf81a9370dbc5bb5f153c866956
Vlad <ejiek@mail.ru>
2015-04-23 11:48:39 +0000

    fixes checkbox value (string to int)

--- new_exam/create.php
+++ new_exam/create.php
@@ -8,11 +8,16 @@
 	mysql_select_db(db) or die(mysql_error());
         
     	$strSQL = "INSERT INTO `" . db . "`.`" . exam_dbt . "` (`id`,`Name`,`author_id`,`is_open`,`comment`) ";
-	    $strSQL = $strSQL .  "VALUES (NULL, '"  . $_POST["name"] . "', '"  . $_POST["author_id"] . "', '"  . $_POST["is_open"] . "', '"  . $_POST["comment"] . "')";
+    	
+    	if($_POST["is_open"] == 'on'){
+    	    $is_open = 1;
+    	}
+    	else{$is_open = 0;}
+	    $strSQL = $strSQL .  "VALUES (NULL, '"  . $_POST["name"] . "', '"  . $_POST["author_id"] . "', '"  . $is_open . "', '"  . $_POST["comment"] . "')";
 	    echo $strSQL;
 	    $rs = mysql_query($strSQL);
 	    
-	    $strSQL = "SELECT * FROM " . exam_dbt . " WHERE Name = '"  . $_POST["name"] . "', author_id = '"  . $_POST["author_id"] . "', is_open = '"  . $_POST["is_open"] . "', comment = '"  . $_POST["comment"] . "')";
+	    $strSQL = "SELECT * FROM " . exam_dbt . " WHERE Name = '"  . $_POST["name"] . "', author_id = '"  . $_POST["author_id"] . "', is_open = '"  . $is_open . "', comment = '"  . $_POST["comment"] . "')";
     }
         
     else{
